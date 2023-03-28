a0f574e56151cf85ce6f6ba0e366104696f3d97d
4ND_M4D <and_mad_@hotmail.com>
2023-03-27 18:00:09 -0600

    update v2
    add funtions

--- js/main.js
+++ js/main.js
@@ -1,41 +1,38 @@
+// Obtiene el elemento HTML con id "cardList"
 const characterAPI = document.getElementById("cardList");
+
+// Hace una solicitud GET a la API de Rick and Morty y obtiene los datos en formato JSON
 let ApiRickAndMorty = fetch("https://rickandmortyapi.com/api/character", {
-    method: "GET"
+  method: "GET"
 }).then(res => {
-    return res.json();
+  return res.json();
 }).then(data => {
-    let characters = data.results;
-    console.log(characters);
-    characters.forEach(character => { 
-        const card = 
-        `<section content="Rick and Morty character: ${character.name}">
-            <div class="firstSectionCard">
-                
-                <h2>${character.name}</h2>
-                
-                <picture>
-                    <img src="${character.image}" alt="${character.name}">
-                </picture>
-
-                <div class="containerStatus">
-                    <p class="characterStatus">${character.status}</p>
-                </div>
-
-                <p class="cardDescription">Species: ${character.species}</p>
-                <p class="cardDescription">Gender: ${character.gender}</p>
-                <p class="cardDescription">Origin: ${character.origin.name}</p>
-                
-            </div>
-<!--    
-            <h3>Capitulos:</h3>
-                <ul>
-                    <li>${character.episode[0]}</li>
-                    <li>${character.episode[1]}</li>
-                    <li>${character.episode[2]}</li>
-                    <li>${character.episode[3]}</li>
-                </ul>
--->        
-        </section>`;
+  // Extraer los personajes de la respuesta de la API
+  let characters = data.results;
+  console.log(characters);
+  // Iterar sobre cada personaje y crear una tarjeta HTML para él
+  characters.forEach(character => { 
+    const card = 
+    `<section content="Rick and Morty character: ${character.name}" data-name="${character.name}" data-species="${character.species}" data-gender="${character.gender}" data-origin="${character.origin.name}">
+      <div class="firstSectionCard">
+        <h2>${character.name}</h2>
+        <picture>
+          <img src="${character.image}" alt="${character.name}">
+        </picture>
+        <div class="containerStatus">
+          <p class="characterStatus">${character.status}</p>
+        </div>
+        <p class="cardDescription">Species: ${character.species}</p>
+        <p class="cardDescription">Gender: ${character.gender}</p>
+        <p class="cardDescription">Origin: ${character.origin.name}</p>
+      </div>
+    </section>`;
+    // Agrega la tarjeta al elemento HTML con id "cardList"
     characterAPI.innerHTML += card;
-    });
+  });
 });
+
+import { searchCharacter } from "./searchCharacter.js";
+// Asignar el evento de carga "load" al elemento de entrada con clase "searchContainer"
+const searchInput = document.querySelector(".searchContainer");
+searchInput.addEventListener("load", searchCharacter);
---
+++ js/searchCharacter.js
@@ -0,0 +1,25 @@
+function searchCharacter() {
+  // Obtener el texto de búsqueda del elemento de entrada
+  const searchValue = document.querySelector(".searchContainer").value.toLowerCase();
+
+  // Obtener todos los elementos de sección (tarjetas de personaje)
+  const sections = document.querySelectorAll("section");
+
+  // Iterar sobre cada sección y ocultarla si no hay una coincidencia con el texto de búsqueda
+  sections.forEach(section => {
+    const name = section.getAttribute("data-name").toLowerCase();
+    const species = section.getAttribute("data-species").toLowerCase();
+    const gender = section.getAttribute("data-gender").toLowerCase();
+    const origin = section.getAttribute("data-origin").toLowerCase();
+    if (name.includes(searchValue) || 
+        species.includes(searchValue) || 
+        gender.includes(searchValue) || 
+        origin.includes(searchValue)) {
+      section.style.display = "block";
+    } else {
+      section.style.display = "none";
+    }
+  });
+}
+
+export { searchCharacter };
